5f333f6f2273136a894cd069a4024f66f3182e25
Marc Christophe <marcchristophe@Marc-Christophes-MacBook-Air.local>
2017-01-10 16:06:39 -0800

    Adds suppression of fci on popup create, re engagement of fci on popup delete

--- init.el
+++ init.el
@@ -352,6 +352,17 @@ you should place your code here."
   (global-fci-mode)
   (add-hook 'Custom-mode-hook (lambda () (turn-off-fci-mode)))
 
+  (defvar sanityinc/fci-mode-suppressed nil)
+  (defadvice popup-create (before suppress-fci-mode activate)
+    "Suspend fci-mode while popups are visible"
+    (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-mode)
+    (when fci-mode
+      (turn-off-fci-mode)))
+  (defadvice popup-delete (after restore-fci-mode activate)
+    "Restore fci-mode when all popups have closed"
+    (when (and (not popup-instances) sanityinc/fci-mode-suppressed)
+      (setq sanityinc/fci-mode-suppressed nil)
+      (turn-on-fci-mode)))
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  neotree
   (setq neo-create-file-auto-open t)
