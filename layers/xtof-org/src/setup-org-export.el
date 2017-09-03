1f2a8da358e81a869b0e76e619c15c4a6810251b
fusupo <fusupo@gmail.com>
2017-09-02 18:07:08 -0700

    adds https protocol to attempt to retrieve disqus widget

--- layers/xtof-org/src/setup-org-export.el
+++ layers/xtof-org/src/setup-org-export.el
@@ -67,7 +67,7 @@
    ;; */
    "(function() { /*DON'T EDIT BELOW THIS LINE*/"
    "var d = document, s = d.createElement('script');"
-   "s.src = 'http://fusupogithubio.disqus.com/embed.js';"
+   "s.src = 'https://fusupogithubio.disqus.com/embed.js';"
    "s.setAttribute ('data-timestamp', +new Date());"
    "(d.head || d.body).appendChild(s);"
    "})();"
